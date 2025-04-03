134c2472f1380f4acc3eddd81590359ff30e6ac7
Legofan123e <111817133+Legofan123e@users.noreply.github.com>
2025-04-03 13:43:59 -0500

    Bot upgrade
    Does 2 new functions: mentioning a new member and rolling a dice (taken from the discord.py library repository)

--- bot.py
+++ bot.py
@@ -2,24 +2,52 @@ import discord
 from discord.ext import commands
 import os, random
 import requests
+
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
+
 bot = commands.Bot(command_prefix='$', intents=intents)
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 @bot.command()
+
 async def hello(ctx):
     await ctx.send(f'Hola, soy un bot {bot.user}!')
 @bot.command()
+
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.group()
+
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
+
 def get_duck_image_url():    
     url = 'https://randomfox.ca/floof/'
     res = requests.get(url)
     data = res.json()
     return data['url']
 @bot.command('duck')
+
 async def duck(ctx):
     '''Una vez que llamamos al comando duck, 
     el programa llama a la función get_duck_image_url'''
